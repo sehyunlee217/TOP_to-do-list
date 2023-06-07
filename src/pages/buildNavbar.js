dc6b3acb5e9ec2fa8ffc8a510d3a35cc9f8042d4
sehyun <sehyun.lee217@gmail.com>
2023-06-07 19:36:30 +0900

    Finished building navbar section and added tailwind css

--- src/pages/buildNavbar.js
+++ src/pages/buildNavbar.js
@@ -1,5 +1,31 @@
 function buildNavbar() {
     console.log("navbar");
+
+    const bodyLoc = document.getElementById("header");
+    console.log(bodyLoc);
+
+    // create navbar
+    const navbar = document.createElement("div");
+    navbar.classList.add("navbar_container");
+
+    // button to toggle sidebar on the left
+    const sidebarBtn = document.createElement("button");
+    sidebarBtn.classList.add("sidebar_btn");
+    sidebarBtn.textContent = "sidebar";
+    // event listener to toggle sidebar btn
+
+    navbar.appendChild(sidebarBtn);
+
+    // To-Do list Title on the right
+    const navbarTxt = document.createElement("span");
+    navbarTxt.textContent = "To-Do List";
+    navbar.appendChild(navbarTxt);
+
+    bodyLoc.appendChild(navbar);
+
+    // ----- navbar tailwind css --------
+    navbar.classList.add("text-white", "text-xl", "font-bold", "flex", "justify-between", "align-center", "px-6", "py-3");
+    bodyLoc.classList.add("bg-blue-300");
 }
 
 export default buildNavbar;
